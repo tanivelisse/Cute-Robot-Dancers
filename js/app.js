daf7cc473af57c4c8e7227662cb7198521f2d4a3
Tania Medina <tanivelisse@gmail.com>
2019-04-05 15:16:49 -0500

    created 2 characters, instatiated robots

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
 console.log("Robot Street Dancer!");
 
-class Robots {
+class Robot {
 	constructor(name) {
-		this.name = name,
+		this.name = name
 		this.coolness = 10
-	},
+	}
 	left() {
 		console.log(`Step to the left`);
-	},
+	}
 	right() {
 		console.log(`step to the right`);
-	},
+	}
 	spin() {
 		console.log;(`I'm spinning!`)
-	},
+	}
 	jump() {
 		console.log('Jump Jump');
-	},
+	}
 	twerk() {
 		console.log(`shake it baby`);
-	},
+	}
 	backFlip() {
 		console.log(`backflip`);
-	},
+	}
 	breakDance() {
 		console.log(`old school!`);
 	}
@@ -30,8 +30,46 @@ class Robots {
 
 
 const game = {
-	robotsAtPlay:[]
+	robotsAtPlay:[],
+	keysPressed: '',
+	chooseCharacter() {
+		console.log('chose x character');
+		if (event.target.id === 'yellowB'){
+   		const yellowBot = new Robot('The yellowB')
+    	this.robotsAtPlay.push(yellowBot);
+    	//console.log(this.robotsAtPlay);
+    	}
+    	if (event.target.id === 'rainbowB'){
+   		const rainbowBot = new Robot('The rainbowB')
+    	this.robotsAtPlay.push(rainbowBot);
+    	//console.log(this.robotsAtPlay);
+    	}
+	},
+	loweringCoolness() {
 
+	},
+	winBattle() {
+
+	}
 
 }
+
+
+//jQuery 
+
+//start button
+
+$('#start').on('click', (e)=> {
+	console.log('Let the games begin!');
+	$('#start').hide("puff");
+	$('#title').hide("puff");
+	$('.characters div').css('display','inline-block');;
+})
+
+$('.characters div').on('click', ()=>{
+	console.log('Character clicked');
+	game.chooseCharacter();
+})
+
+
 
